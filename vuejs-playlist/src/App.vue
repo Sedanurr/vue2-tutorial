5847f021fc8a8036ec2ace311634295247daaef2
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-10 22:47:32 +0300

    tutorial17&18

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+  <h1>{{title}}</h1>
+  <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title:'Your first Vue file,wooo!'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'heeyyyy cowboy!'
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
